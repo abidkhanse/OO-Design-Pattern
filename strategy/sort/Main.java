0cb8dd691552ecf2a5a9bd95ca11e6681a51f1b2
abid khan <abikhase@gmail.com>
2023-07-23 00:32:37 +0200

    Quick and Bubble sort is implemented

--- strategy/sort/Main.java
+++ strategy/sort/Main.java
@@ -1,31 +1,35 @@
 package strategy.sort;
 
 import strategy.sort.types.Bubble;
-import strategy.sort.types.Merge;
+import strategy.sort.types.Quick;
 import strategy.sort.types.Selection;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int arr[] = new int[5];
-        arr[0] = 5;
-        arr[1] = 1;
-        arr[2] = 3;
-        arr[3] = 1;
-        arr[4] = 1;
+        int arr[] = new int[6];
+        arr[0] = 24;
+        arr[1] = 9;
+        arr[2] = 29;
+        arr[3] = 14;
+        arr[4] = 39;
+        arr[5] = 3;
 
         SortingStrategy strategy = new Bubble();
         SortContext sortContext = new SortContext(strategy);
         sortContext.perform(arr);
 
-        strategy = new Merge();
+        strategy = new Quick();
         sortContext.changeSort(strategy);
         sortContext.perform(arr);
 
         strategy = new Selection();
         sortContext.changeSort(strategy);
         sortContext.perform(arr);
+
+        String ss = "Hello";
+
 
     }
 }
--- strategy/sort/types/Bubble.java
+++ strategy/sort/types/Bubble.java
@@ -7,6 +7,23 @@ import java.util.Arrays;
 public class Bubble implements SortingStrategy {
     @Override
     public void sort(int[] array) {
-        System.out.println("Bubble sort implemented");
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if(array[i] > array[j]) {
+                    swap(array, i, j);
+                }
+            }
+        }
+
+        System.out.println("Bubble sort implemented" + Arrays.toString(array));
+
     }
+
+    void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 }
